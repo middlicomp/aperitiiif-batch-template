f0a4635a73373baa5a9252e435b43abcd5289c51
mnyrop <marii@nyu.edu>
2022-06-29 17:22:28 -0400

    convert to tif

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
 
-gem 'vips'
+gem 'ruby-vips'
--- lib/ingest.rb
+++ lib/ingest.rb
@@ -9,9 +9,17 @@ NEW_SRC              = 'tmp'
 FileUtils.mkdir_p NEW_SRC
 
 files = Dir.glob("#{SRC_DIR}/**/*").select { |f| File.file? f }
-files.each do |p|
-  base = p.gsub(SRC_DIR, '').split('/').reject(&:empty?)
-  namespaced = base.prepend(COLLECTION_NAMESPACE).join('_')
+files.each do |f|
+  base_path     = f.gsub(SRC_DIR, '').split('/').reject(&:empty?)
+  renamed_file  = "#{NEW_SRC}/#{base_path.prepend(COLLECTION_NAMESPACE).join('_')}"
 
-  FileUtils.cp p, "#{NEW_SRC}/#{namespaced}"
+  FileUtils.cp f, renamed_file
+
+  image   = Vips::Image.new_from_file renamed_file
+  tiffile = renamed_file.gsub(File.extname(renamed_file), '.tif')
+
+  image.write_to_file tiffile
+  FileUtils.rm renamed_file
+
+  puts "Generated #{tiffile}"
 end
